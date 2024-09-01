d1e4a3f966bdd5963b83d4aa782d4b733df17c44
denyskh4991 <denyskh4991@gmail.com>
2024-09-01 17:58:07 +0200

    Adding hiding the list before clicking

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let selectField = document.getElementById("selectField");
 let selectText = document.getElementById("selectText");
 let options = document.getElementsByClassName("options");
+let list = document.getElementById("list");
+
+selectField.onclick = function () {
+    list.classList.toggle("hide");
+}
 
 for (option of options) {
     option.onclick = function () {
